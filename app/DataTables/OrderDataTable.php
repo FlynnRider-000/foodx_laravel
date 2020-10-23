24c717f6988ccc8cac98b5b0f389776f1c5b897c
FlynnRider-000 <coronarider01@gmail.com>
2020-10-24 00:35:22 +1000

    column fix

--- app/DataTables/OrderDataTable.php
+++ app/DataTables/OrderDataTable.php
@@ -135,7 +135,7 @@ class OrderDataTable extends DataTable
                 'title' => trans('lang.order_updated_at'),
                 'searchable' => false,
                 'orderable' => true,
-            ]
+            ],
             [
                 'data' => 'created_at',
                 'title' => trans('lang.order_created_at'),
